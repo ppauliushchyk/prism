7c915687058daad76198ca8e798b7506b2343011
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-12-08 15:33:17 +0200

    test: :test_tube: added tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,78 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from "../src/app.module";
+import { engine } from "express-handlebars";
+import { INestApplication } from "@nestjs/common";
+import { join } from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from "supertest";
 
-describe('AppController (e2e)', () => {
+describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication<NestExpressApplication>();
+
+    // @ts-expect-error valid
+    app.engine(
+      "hbs",
+      engine({
+        defaultLayout: "index",
+        extname: "hbs",
+        layoutsDir: join(__dirname, "..", "views", "layouts"),
+        partialsDir: join(__dirname, "..", "views", "partials"),
+      }),
+    );
+    // @ts-expect-error valid
+    app.setViewEngine("hbs");
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe("/", () => {
+    it("should render", async () => {
+      const response = await request(app.getHttpServer()).get("/");
+
+      expect(response.status).toBe(200);
+      expect(response.text).toContain("Payment Form");
+    });
+  });
+
+  describe("/payment", () => {
+    describe("/cancelled", () => {
+      it("should render", async () => {
+        const response = await request(app.getHttpServer()).get(
+          "/payment/cancelled",
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.text).toContain("Payment Cancelled");
+      });
+    });
+
+    describe("/failed", () => {
+      it("should render", async () => {
+        const response = await request(app.getHttpServer()).get(
+          "/payment/failed",
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.text).toContain("Payment Failed");
+      });
+    });
+
+    describe("/successful", () => {
+      it("should render", async () => {
+        const response = await request(app.getHttpServer()).get(
+          "/payment/successful",
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.text).toContain("Payment Successful");
+      });
+    });
   });
 });
