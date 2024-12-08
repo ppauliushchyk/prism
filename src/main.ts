cc01c856251fe969e5927eb20a63ba183dad4c77
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-12-08 15:32:02 +0200

    feat: 📺 configured view engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
+import { join } from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { NestFactory } from "@nestjs/core";
+import helmet from "helmet";
+import { engine } from "express-handlebars";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ["log", "error", "warn", "debug", "verbose", "fatal"],
+  });
+
+  app.enableCors();
+  app.use(helmet());
+
+  app.useStaticAssets(join(__dirname, "..", "public"));
+  app.setBaseViewsDir(join(__dirname, "..", "views"));
+
+  app.engine(
+    "hbs",
+    engine({
+      defaultLayout: "index",
+      extname: "hbs",
+      layoutsDir: join(__dirname, "..", "views", "layouts"),
+      partialsDir: join(__dirname, "..", "views", "partials"),
+    }),
+  );
+
+  app.setViewEngine("hbs");
+
+  app.set("view options", { layout: "layout" });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
