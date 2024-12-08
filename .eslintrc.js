8b5d1f128820968f71c47b6f4f73e91afce0c3a4
Pavel Pauliushchyk <ppauliushchyk@gmail.com>
2024-12-09 01:09:26 +0200

    build: 📏 updated linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "plugin:import/recommended",
   ],
   ignorePatterns: [".eslintrc.js"],
   parser: "@typescript-eslint/parser",
@@ -18,5 +19,23 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "import/extensions": "off",
+    "import/no-unresolved": "off",
+    "import/order": [
+      "error",
+      {
+        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "import/prefer-default-export": "off",
   },
 };
